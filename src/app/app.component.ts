900d5f248f2dd93db84f6582b1697225d336b303
Paul Hellweg <49630512+PaulHellweg@users.noreply.github.com>
2022-10-26 11:25:01 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'craftworks-simple-task-management-systesm';
+}
